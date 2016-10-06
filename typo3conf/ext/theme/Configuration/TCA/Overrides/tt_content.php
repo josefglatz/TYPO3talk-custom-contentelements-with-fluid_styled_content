e62b58b0b8a86d4ddf315e25476e77cc20d0b558
Josef Glatz <josefglatz@gmail.com>
2016-10-06 12:17:50 +0200

    2b. Add the desired TCA configuration for the new content element
    Our content element should have three properties among the common used properties:
    - header
    - subheader
    - image (with additional label "Slider Images")
    
    Likewise, the editor will be forced to fill in content in header and subheader. The
    editor must provide a minimum of 1 image but should only be allowed to add a maximum
    of 4 images.
    
    If I write the TCA configuration for content elements, it's common for me, to copy
    an already existing tca configuration of a default content element like "header". You
    find existing configuration within the TYPO3 core at https://github.com/TYPO3/TYPO3.CMS/blob/7254650107f48732b963d5ef8c6838bdd02ff1bd/typo3/sysext/frontend/Configuration/TCA/tt_content.php#L1136
    
    line 20ff) All my modifications are (temporary) in the variable "$tca"
    line 55!!!) $GLOBALS['TCA']['tt_content'] gets finally gets finally replaced by all my previous configuration
    line 22) I add my new type "theme_tuga"
    line 23) The showitem was initially copied from an existing core content element and modified to our needs.
    line 25-27) the specific properties of our content element
    
    line 34-51) via the columnOverrides it's possible to override columns/properties especially for our content element.
    This is also possible via TSConfig - but I prefer to put things together if it's possible.
    
    Now you should be able to edit your new content element in the backend (after clearing TYPO3 caches).
    
    ツ

--- typo3conf/ext/theme/Configuration/TCA/Overrides/tt_content.php
+++ typo3conf/ext/theme/Configuration/TCA/Overrides/tt_content.php
@@ -16,6 +16,44 @@ call_user_func(
             $extKey
         );
 
+
+        $tca = [
+            'types' => [
+                'theme_tuga' => [
+                    'showitem' => '
+                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                        header,
+                        subheader,
+                        image;Slider Images,
+                        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended,
+                    ',
+                    'columnsOverrides' => [
+                        'header' => [
+                            'config' => [
+                                'eval' => 'required'
+                            ]
+                        ],
+                        'subheader' => [
+                            'config' => [
+                                'eval' => 'required'
+                            ]
+                        ],
+                        'image' => [
+                            'config' => [
+                                'maxitems' => '4',
+                                'minitems' => '1'
+                            ]
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $GLOBALS['TCA']['tt_content'] = array_replace_recursive($GLOBALS['TCA']['tt_content'], $tca);
+
     },
     'theme'
 );
